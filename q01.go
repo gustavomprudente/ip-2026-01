b92e1118f478d30bbb41bf35cc1206575d0fcc10
GUSTAVO MENDONÇA PRUDENTE <gustavoprudente@discente.ufg.br>
2026-03-20 01:50:12 -0300

    questão 01

---
+++ q01.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n1, n2, n3 float32
+	fmt.Printf("Digite 3 números:") 
+
+	fmt.Scan(&n1, &n2, &n3)
+
+	media := (n1 + n2 + n3)/3
+
+	fmt.Printf("MEDIA = %.2f\n", media)
+
+	if media >= 6 {
+		fmt.Println("APROVADO")
+	} else {
+	fmt.Println("REPROVADO")
+	}
+	
+}
