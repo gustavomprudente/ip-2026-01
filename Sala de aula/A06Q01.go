a44efe8cc8481dc4308bb066ef83e4244158f615
Gustavo Mendonça Prudente <gustavoprudente@discente.ufg.br>
2026-03-30 21:35:36 -0300

    Implement grade input and average calculation

--- Sala de aula/A06Q01.go
+++ Sala de aula/A06Q01.go
@@ -1,1 +1,26 @@
+package main 
 
+import "fmt"
+
+func main () {
+
+	var (
+		nota [3]float64
+		media, soma float64
+	)
+	for i := 0; i < 3; i++ {
+		fmt.Printf ("Digite a nota %d: ", i+1)
+		fmt.Scan(&nota[i])
+		soma += nota[i]
+	} 
+	media = soma / 3
+	fmt.Printf("Média: %.2f\n", media)
+
+	for i := range nota {
+		if nota[i] >= 6 {
+			fmt.Printf("A %d° nota está acima da média (%.2f)\n", i + 1, nota[i])
+		} else {
+			fmt.Printf("A %d° nota não está acima da média (%.2f)\n", i+1, nota[i])
+		}
+	}
+}
