c2b03e7efa6c21e40c82b6bf4d75d4f5e976f4ec
Gustavo Mendonça Prudente <gustavoprudente@discente.ufg.br>
2026-03-30 21:55:52 -0300

    Update Q02.go

--- Sala de aula/AULA06/Q02.go
+++ Sala de aula/AULA06/Q02.go
@@ -1,1 +1,16 @@
+package main
 
+import "fmt"
+
+func main () {
+	var v [5]float64
+	var soma float64
+
+	for i:= 0; i < 5; i++ {
+		fmt.Printf("Digite o %d° número:", i+1)
+		fmt.Scan(&v[i])
+		soma += v[i]
+	}
+	
+	fmt.Printf("A soma desses números é igual a: %.2f\n", soma)
+}
